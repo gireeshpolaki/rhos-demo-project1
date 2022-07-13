63292e6b7cef5050d2d9df87d7fb9bc3097e71c2
Gireesh Polaki <gireesh.polaki@gmail.com>
2022-07-13 10:19:06 +0530

    changed target port 8080

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = 3000;
+const port = 8080;
 
 app.get("/hello", (req, res) => {
   const name = req.query.name || "Hello World";
